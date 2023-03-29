218cd24a9aa69b3285c71c935ba44c4b4c85f49e
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-29 10:03:25 -0400

    pylint and flake8 will now pass

--- {{ cookiecutter.project_slug }}/users/forms.py
+++ {{ cookiecutter.project_slug }}/users/forms.py
@@ -4,10 +4,10 @@ from {{cookiecutter.project_slug}}.utils import DivErrorList  # noqa: E999
 
 class CustomSignUpForm(SignupForm):
     def __init__(self, *args, **kwargs):
-      super(CustomSignUpForm, self).__init__(*args, **kwargs)
+      super().__init__(*args, **kwargs)
       self.error_class = DivErrorList
 
 class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
-      super(CustomLoginForm, self).__init__(*args, **kwargs)
+      super().__init__(*args, **kwargs)
       self.error_class = DivErrorList
