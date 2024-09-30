744557db096e07f07044309ebee4fcd450592571
Rasul Kireev <me@rasulkireev.com>
2024-09-30 12:49:24 +0300

    changes

--- {{ cookiecutter.project_slug }}/core/models.py
+++ {{ cookiecutter.project_slug }}/core/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from core.base_models import BaseModel
+from core.utils import generate_random_key
 
 
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=10, unique=True, default=generate_random_key)
--- {{ cookiecutter.project_slug }}/core/signals.py
+++ {{ cookiecutter.project_slug }}/core/signals.py
@@ -1,9 +1,12 @@
+from allauth.account.signals import email_confirmed
+from allauth.socialaccount.signals import social_account_added
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_q.tasks import async_task
 
+from core.tasks import add_email_to_buttondown
 from core.models import Profile
-
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -17,6 +20,22 @@ def save_user_profile(sender, instance, **kwargs):
 
 {% if cookiecutter.use_buttondown == 'y' -%}
 @receiver(email_confirmed)
+{% if cookiecutter.use_social_auth == 'y' -%}
+@receiver(social_account_added)
+{% endif -%}
 def email_confirmation_callback(sender, **kwargs):
-    async_task(add_email_to_buttondown, kwargs["email_address"], tag="user")
+    if 'email_address' in kwargs:
+        # This is for email_confirmed signal
+        email = kwargs['email_address']
+    {% if cookiecutter.use_social_auth == 'y' -%}
+    elif 'sociallogin' in kwargs:
+        # This is for social_account_added signal
+        email = kwargs['sociallogin'].user.email
+    {% endif -%}
+    else:
+        # If neither email_address nor sociallogin is present, we can't proceed
+        return
+
+    if email:
+        async_task(add_email_to_buttondown, email, tag="user")
 {% endif -%}
