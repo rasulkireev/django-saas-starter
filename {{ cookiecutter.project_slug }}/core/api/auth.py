199a9a26f17081bac5c64952dc9fabc38a27cc67
Rasul Kireev <me@rasulkireev.com>
2025-08-18 11:47:10 +0300

    Get the token for API call authentication, before authenticating

--- {{ cookiecutter.project_slug }}/core/api/auth.py
+++ {{ cookiecutter.project_slug }}/core/api/auth.py
@@ -1,31 +1,50 @@
-from typing import Optional
-
 from django.http import HttpRequest
 from ninja.security import HttpBearer
 
 from core.models import Profile
 
+from {{ cookiecutter.project_slug }}.utils import get_{{ cookiecutter.project_slug }}_logger
+
+logger = get_{{ cookiecutter.project_slug }}_logger(__name__)
+
 
 class MultipleAuthSchema(HttpBearer):
-    def authenticate(self, request: HttpRequest, token: Optional[str] = None) -> Optional[Profile]:
-        # For session-based authentication (when using the web interface)
-        if hasattr(request, "user") and request.user.is_authenticated:
+    def authenticate(self, request: HttpRequest, token: str | None = None) -> Profile | None:
+        if token:
+            logger.info(
+                "[Django Ninja Auth] API Request with token",
+                request=request.__dict__,
+                token=token,
+            )
             try:
-                return request.user.profile
+                return Profile.objects.get(key=token)
             except Profile.DoesNotExist:
                 return None
 
-        # For API token authentication (when using the API directly)
-        if token:
+        if hasattr(request, "user") and request.user.is_authenticated:
+            logger.info(
+                "[Django Ninja Auth] API Request with user",
+                request=request.__dict__,
+                user=request.user.__dict__,
+            )
             try:
-                return Profile.objects.get(key=token)
+                return request.user.profile
             except Profile.DoesNotExist:
                 return None
 
         return None
 
     def __call__(self, request):
-        # Override to make authentication optional for session-based requests
+        logger.info(
+            "[Django Ninja Auth] API Request",
+            request=request.__dict__,
+        )
+
+        authorization = request.headers.get("Authorization", "")
+        if authorization.startswith("Bearer "):
+            token = authorization.split(" ")[1]
+            return self.authenticate(request, token)
+
         if hasattr(request, "user") and request.user.is_authenticated:
             return self.authenticate(request)
 
